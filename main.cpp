5689be98d5c71a640c1b805267b597416fa7ad7b
Vavilov1853 <vavilov.andrey1853@gmail.com>
2016-09-16 16:53:22 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,15 @@
-#include <iostream>
-
 #include <stdio.h>
 #include <math.h>
-int main()
-{
+
+int main() {
     float G, F, Y, x, a;
-    const double pi=3.14;
+    const double pi = 3.14;
     scanf("%f", &x);
     scanf("%f", &a);
 
-    G=(5*(10*pow(a,2)-11*a*x+pow(x,2)))/(24*pow(a,2)-49*a*x+4*pow(x,2));
-    F=sinh(3*pow(a,2)+7*a*x+4*pow(x,2));
-    Y=tanh(30*pow(a,2)+37*a*x-4*pow(x,2));
+    G = (5 * (10 * pow(a, 2) - 11 * a * x + pow(x, 2))) / (24 * pow(a, 2) - 49 * a * x + 4 * pow(x, 2));
+    F = sinh(3 * pow(a, 2) + 7 * a * x + 4 * pow(x, 2));
+    Y = tanh(30 * pow(a, 2) + 37 * a * x - 4 * pow(x, 2));
 
     printf("%1.3f\n", G);
     printf("%1.3f\n", F);
