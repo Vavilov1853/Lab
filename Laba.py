f10f9c835d308a2de4e399f807bf456618df0ab5
Vavilov1853 <vavilov.andrey1853@gmail.com>
2016-09-16 17:24:26 +0300

    Update Laba.py

--- Laba.py
+++ Laba.py
@@ -1,7 +1,12 @@
 import math
+
 x = int(input('Введите x='))
 a = int(input('Введите a='))
-Y = math.arctan(30 * a ** 2 + 37 * a * x - 4 * x ** 2)
+
+Y = math.atan(30 * a ** 2 + 37 * a * x - 4 * x ** 2)*-1
 F = math.sinh(3 * a ** 2 + 7 * a * x + 4 * x ** 2)
 G = 5*(10*a**2-11*a*x+x**2)/(24*a**2-49*a*x+15*x**2)
-print(Y,F,G)
+
+print(G)
+print(F)
+print(Y)
