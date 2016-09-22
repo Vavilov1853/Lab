eecda8014bba73ec207af80ae978a1eb9162486e
Vavilov1853 <vavilov.andrey1853@gmail.com>
2016-09-22 19:50:28 +0300

    Add files via upload

---
+++ Laba2/Var31.py
@@ -0,0 +1,25 @@
+import math
+pi=3.14
+x = int(input("Введите x:"))
+a = int(input("Введите a:"))
+z = str(input("Введите букву,которую xотите вычислить"))
+F = 0
+G = 0
+Y = 0
+if z == "G":
+    G = 5 * (10 * a ** 2 - 11 * a * x + x ** 2) / (24 * a ** 2 - 49 * a * x + 15 * x ** 2)
+    print("G =", G)
+elif z == "Y":
+    Y = math.atan(30 * a ** 2 + 37 * a * x - 4 * x ** 2) * -1
+    if Y:
+        Y_fun = math.atan(Y)
+    print("Y =", Y)
+
+elif z == "F":
+    F = math.sinh(3 * a ** 2 + 7 * a * x + 4 * x ** 2)
+    if F:
+        F_fun = math.sin(F)
+        print("F =", F)
+    else: print("Не существует синуса к заданной функции")
+else:
+    print("Введите букву функции, которую xотите вычислить")
