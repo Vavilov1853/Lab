7321fd4c16fab2431b2a286531ee26f3fa9accd5
Vavilov1853 <vavilov.andrey1853@gmail.com>
2016-10-11 17:32:51 +0400

    Update main.c

--- Laba3/main.c
+++ Laba3/main.c
@@ -5,7 +5,7 @@ int main() {
     double G, F, Y, x, a, x2, b;
     int r;
 
-    printf("%s", "Введите x: ");
+    printf("%s", "Введите  x: ");
     scanf("%lf", &x);
     getchar();
 
@@ -17,7 +17,7 @@ int main() {
     scanf("%lf", &x2);
     getchar();
 
-    printf("%s", "Введите b ");
+    printf("%s", "Введите параметр шага  ");
     scanf("%lf", &b);
     getchar();
 
@@ -28,34 +28,32 @@ int main() {
     switch (r) {
         case 1:
             for (; x < x2; x = x + b) {
-                if ((G <= 0.00001) || (G >= 0.00001)) { 
+                if ((G <= 0.00001) || (G >= 0.00001)) {
                     G = ((5 * (10 * pow(a, 2) - 11 * a * x + pow(x, 2))) /
                          (24 * pow(a, 2) - 49 * a * x + 4 * pow(x, 2)));
                     printf("%lf \n", G);
                 } else printf("%s\n", "При данном значение G выполнение функции невозможно");
-            }
 
+            }
             break;
+        Metka:
         case 2:
             for (; x >= x2; x = x - b) {
                 F = sin(3 * pow(a, 2) + 7 * a * x + 4 * pow(x, 2));
-                printf("F = %lf \n", F);
+                printf("%lf \n", F);
             }
-            break;
-        case 3 :
+            if()
+
+                printf("%s" 'Введите 3 если хотите запустить заново' );
+            goto Metka;
+
+         case 3 :
             for (; x <= x2; x = x + b) {
                 if ((Y > -1) && (Y < 1)) {
                     Y = tan(30 * pow(a, 2) + 37 * a * x - 4 * pow(x, 2));
                 } else printf("%s\n", "При данном значение Y выполнение функции невозможно");
-                printf("Y = %lf \n", Y);
+                printf("%lf \n", Y);
             }
-            break;
-        default:
-            printf("%s\n", "Для выполнения действия напишите 1,2 или 3");
-
-            return 0;
-    }
-}
             break;
         default:
             printf("%s\n", "Для выполнения действия напишите 1,2 или 3");
