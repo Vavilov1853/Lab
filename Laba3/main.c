1b8359a6f4625d6f0194a4fd0f76074611d792c5
Vavilov1853 <Vavilov.andrey1853@gmail.com>
2016-10-20 16:13:34 +0300

    Продолжил работу над 3 лабораторной

--- Laba3/main.c
+++ Laba3/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 int main() {
-    double G, F, Y, x, a, x2, b ;
+    double G, F, Y, x, a, x2, b, function, function2, difference ;
     int r;
     int change;
     Metka:
@@ -20,6 +20,10 @@ int main() {
     scanf("%lf", &b);
     getchar();
 
+    printf("%s", "Введите разницу между функциями ");
+    scanf("%lf", &difference);
+    getchar();
+
 
     printf("Введите 1 для вызова G, введите 2 для вызова F, введите 3 для вызова Y  \n");
     scanf("%i", &r);
@@ -28,9 +32,18 @@ int main() {
         case 1:
             for (; x < x2; x = x + b) {
                 if ((G <= 0.00001) || (G >= 0.00001)) {
-                    G = ((5 * (10 * pow(a, 2) - 11 * a * x + pow(x, 2))) /
-                         (24 * pow(a, 2) - 49 * a * x + 4 * pow(x, 2)));
-                    printf("%lf \n", G);
+                    function = ((5 * (10 * pow(a, 2) - 11 * a * x + pow(x, 2))) /
+                                (24 * pow(a, 2) - 49 * a * x + 15 * pow(x, 2)));
+                    printf("G = %lf \n", function);
+                    function2 = ((5 * (10 * pow(a, 2) - 11 * a * (x + b) + pow(x + b, 2))) /
+                                 (24 * pow(a, 2) - 49 * a * (x + b) + 4 * pow((x + b), 2)));
+
+                    if ((function2 - function < difference)) {
+                        b *= 2;
+
+                        function2 = ((5 * (10 * pow(a, 2) - 11 * a * (x + b) + pow((x + b), 2))) /
+                                     (24 * pow(a, 2) - 49 * a * (x + b) + 4 * pow((x + b), 2)));
+                    }
                 } else printf("%s\n", "При данном значение G выполнение функции невозможно");
 
             }
@@ -41,9 +54,15 @@ int main() {
                 goto Metka;
             break;
         case 2:
-            for (; x >= x2; x = x - b) {
-                F = sin(3 * pow(a, 2) + 7 * a * x + 4 * pow(x, 2));
-                printf("%lf \n", F);
+            for (; x >= x2; x = x + b) {
+                if ((F >= -1) && (F <= 1))
+                    function = sin(3 * pow(a, 2) + 7 * a * x + 4 * pow(x, 2));
+                printf("F = %lf \n", function);
+                function2 = sin(3 * pow(a, 2) + 7 * a * (x + b) + 4 * pow((x + b), 2));
+
+                if ((function2 - function > difference))
+                    b /= 3;
+                function2 = sin(3 * pow(a, 2) + 7 * a * (x + b) + 4 * pow((x + b), 2));
 
                 printf("Если желаете посчитать другую функцию введите 1 \n");
                 scanf("%d",& change);
@@ -54,11 +73,16 @@ int main() {
             break;
         case 3 :
             for (; x <= x2; x = x + b) {
-                if ((Y > -1) && (Y < 1)) {
-                    Y = tan(30 * pow(a, 2) + 37 * a * x - 4 * pow(x, 2));
-                } else printf("%s\n", "При данном значение Y выполнение функции невозможно");
-                printf("%lf \n", Y);
-            }
+                if (((30 * pow(a, 2) + 37 * a * x - 4 * pow(x, 2)) * -1 > -1) || ((30 * pow(a, 2) + 37 * a * x - 4 * pow(x, 2)) * -1 < 1)) {
+                    function = tan(30 * pow(a, 2) + 37 * a * x - 4 * pow(x, 2)) * -1;
+                    printf("%lf \n", Y);
+                    function2 = tan(30 * pow(a, 2) + 37 * a * (x + b) - 4 * pow((x + b), 2)) * -1;
+
+                    if ((function2 - function > difference));
+                        b *=2;
+
+                    } else printf("%s\n", "При данном значение Y выполнение функции невозможно");
+                }
 
             printf("Если желаете посчитать другую функцию введите 1 \n");
             scanf("%d",& change);
