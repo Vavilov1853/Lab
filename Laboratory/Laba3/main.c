9de85c0326279ea87be0b9df89d506bd6c0d471c
Vavilov1853 <vavilov.andrey1853@gmail.com>
2016-11-11 18:51:40 +0400

    Update main.c

--- Laboratory/Laba3/main.c
+++ Laboratory/Laba3/main.c
@@ -43,14 +43,13 @@ int main() {
                              (24 * pow(a, 2) - 49 * a * (x + b) + 4 * pow((x + b), 2)));
 
                 if (function2 - function > difference) {
-
+                    b *= 2;
+                    function2 = ((5 * (10 * pow(a, 2) - 11 * a * (x + b) + pow(x + b, 2))) /
+                                 (24 * pow(a, 2) - 49 * a * (x + b) + 4 * pow((x + b), 2)));
                     {
                         if (x >= x2)
                             break;
                     }
-                    b *= 2;
-                    function2 = ((5 * (10 * pow(a, 2) - 11 * a * (x + b) + pow(x + b, 2))) /
-                                 (24 * pow(a, 2) - 49 * a * (x + b) + 4 * pow((x + b), 2)));
                     break;
                 }
             }
@@ -86,8 +85,10 @@ int main() {
 
         case 3:
             while (x < x2, x = x + b) {
-                if (function > -1 && function < 1) {
+                if ((function > -1) || (function < 1)) {
+
                     function = sin(3 * pow(a, 2) + 7 * a * x + 4 * pow(x, 2));
+                    x = x + b;
                     printf("%f\n", function);
 
                     if (function2 - function > difference)
@@ -97,16 +98,16 @@ int main() {
                 if (x >= x2) {
                     break;
                 }
-                printf("Если желаете посчитать другую функцию введите 1 \n");
-                scanf("%d", &change);
-                getchar();
-                if (change == 1)
-                    goto Metka;
-                break;
-                default:
-                    printf("Для выполнения действия напишите 1,2 или 3");
-                break;
             }
-            return 0;
+            printf("Если желаете посчитать другую функцию введите 1 \n");
+            scanf("%d", &change);
+            getchar();
+            if (change == 1)
+                goto Metka;
+            break;
+        default:
+            printf("Для выполнения действия напишите 1,2 или 3");
+            break;
     }
+    return 0;
 }
