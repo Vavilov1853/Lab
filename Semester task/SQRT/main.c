a7561f75357621bbe382008d73d6f8202620f079
Vavilov1853 <vavilov.andrey1853@gmail.com>
2017-01-10 01:14:06 +0300

    +

--- Semester task/SQRT/main.c
+++ Semester task/SQRT/main.c
@@ -3,7 +3,7 @@
 int main()
 {
     double a, x;
-    int iterator = 0;
+    int i = 0;
 
     printf("Введите число: ");
     scanf("%lf",&a);
@@ -12,8 +12,8 @@ int main()
     while (x*x!=a)
     {
         x = 0.5 * (x + (a / x));
-        iterator++;
-        if (iterator>150)
+        i++;
+        if (i>200)
             break;
     }
     printf("Квардратный корень из числа %f равен: %f",a,x);
