3990319fef7516eb58d026e96325952820f266b9
Vavilov1853 <vavilov.andrey1853@gmail.com>
2017-01-10 00:20:57 +0300

    +

--- Semester task/SQRT/main.c
+++ Semester task/SQRT/main.c
@@ -1,20 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 
 int main() {
-    double a, x;
-    int i;
+    int i,A[20],n=3,p[20],j;
+    double EMA[20],m;
+    m =(n+1);
+    m = 2/m;
+    FILE *v;
+    v = fopen("//home/user/ClionProjects/SQRT/text.dat", "w+");
 
-    printf("Введите число: ");
-    scanf("%lf", &a);
-
-    x = a;
-
-        while (((x * x > a)) && (i < 200)) {
-            x = 0.5 * (x + (a / x));
-            i++;
-            break;
-        }
-        printf("Квардратный корень из числа %f равен: %f", a, x);
-
-        return 0;
+    for (i = 0; i < 20; i++)
+    {
+        A[i] = rand()%100;
     }
+
+    p[0]= A[0];
+    for (i = 0;i < 20; i++){
+        if (i - n == 0){
+            for (j = i-n; j < i ; ++j) {
+                if (j>=1) {
+                    p[j] = A[j] + p[j - 1];
+                }
+            }
+        }
+        if (i == n) {
+            EMA[i] = p[j-1]/n; /*по сути находим обычную среднюю для первого значения*/
+        }
+        if (i > n) {
+            EMA[i] = m * A[i] + (1 - m) * EMA[i - 1];
+
+        }
+        printf("%lf\n",EMA[i]);
+    }
+
+    for(i = 0,j = n;i < 20,j<20;i +=1,j+=1){
+        fprintf(v, "%i %i %lf \n", i , A[i], EMA[j]);
+    }
+
+    fclose(v);
+
+    return 0;
+}
