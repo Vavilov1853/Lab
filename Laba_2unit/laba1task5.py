04e3924514c7949a58e4c1f90a1d9ce0c17700dd
Misha91908 <misha91908@gmail.com>
2018-05-03 12:51:16 +0300

    Обновлена 5 лаба

--- Laba_2unit/laba1task5.py
+++ Laba_2unit/laba1task5.py
@@ -4,5 +4,36 @@ import argparse
 
 def File_read():
     f = open('mbox.txt','r')
+    output = []
     for line in f:
-        
+        if line != '':
+            output.append(line)
+    return (output)
+def Spam(data: list):
+
+    step = 0
+    output = 0.0
+    for line in data:
+
+        if line.find('X-DSPAM-Confidence: ') != -1:
+            step +=1
+
+            value = float(line.split('X-DSPAM-Confidence: ')[1])
+            output += value
+
+            output /= step
+    return output
+
+def fenders(data: list):
+
+    step = 0
+
+    for line in data:
+
+        if line.find('From: ')
+
+
+if __name__ == '__main__':
+    val = File_read()
+    print(Spam(val))
+    print()
